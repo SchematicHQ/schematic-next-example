0257ab2f70adeefc8b07a1d73edaa48e640e32bd
Ben Papillon <benpapillon@gmail.com>
2024-09-10 15:05:54 -0400

    fix eslint issue

--- src/app/api/accessToken/route.ts
+++ src/app/api/accessToken/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { SchematicClient } from "@schematichq/schematic-typescript-node";
 import { auth, clerkClient } from "@clerk/nextjs/server";
 
+/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 export async function GET(_request: NextRequest) {
   const apiKey = process.env.SCHEMATIC_SECRET_KEY;
   if (!apiKey) {
