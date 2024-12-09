75e61677d1275ff88435b65071cf79ee44fae770
Ben Papillon <benpapillon@gmail.com>
2024-12-09 12:15:52 -0500

    Pinned locations

---
+++ src/app/api/pins/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server";
+import { SchematicClient } from "@schematichq/schematic-typescript-node";
+import { getAuthOrgId, AuthError } from "../../../utils/auth";
+import { clerkClient } from "@clerk/nextjs/server";
+
+/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+export async function POST(request: NextRequest) {
+  const apiKey = process.env.SCHEMATIC_SECRET_KEY;
+  if (!apiKey) {
+    return NextResponse.json({ message: "No Schematic key" }, { status: 400 });
+  }
+
+  const { locations } = await request.json();
+  const numLocations = locations?.length ?? 0;
+
+  try {
+    const { orgId } = await getAuthOrgId();
+    const schematicClient = new SchematicClient({ apiKey });
+
+    await schematicClient.companies.upsertCompany({
+      keys: { clerkId: orgId },
+      traits: { numLocations },
+    });
+
+    await clerkClient.organizations.updateOrganizationMetadata(orgId, {
+      publicMetadata: { locations },
+    });
+
+    return NextResponse.json({ numLocations });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      return NextResponse.json(
+        { message: (error as AuthError).message },
+        { status: 400 },
+      );
+    }
+
+    return NextResponse.json(
+      { message: "Failed to update location count" },
+      { status: 500 },
+    );
+  }
+}
