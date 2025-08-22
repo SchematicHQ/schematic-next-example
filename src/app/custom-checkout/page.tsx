297c251c34fa5cf948e7e1dce18009daa3a0e0a4
Cole Chrzan <cole@schematichq.com>
2025-08-22 01:06:01 -0400

    chore: cleanup

--- src/app/custom-checkout/page.tsx
+++ src/app/custom-checkout/page.tsx
@@ -8,6 +8,7 @@ import {
   useEmbed,
 } from "@schematichq/schematic-components";
 import { useCallback, useEffect, useState } from "react";
+import { embedSettings } from "../embedSettings";
 
 function Checkout() {
   const [isLoading, setIsLoading] = useState(false);
@@ -59,72 +60,7 @@ function Checkout() {
 
 export default function CustomCheckout() {
   return (
-    <EmbedProvider
-      settings={{
-        theme: {
-          numberOfColumns: 1,
-          colorMode: "dark",
-          primary: "#2bbde1",
-          secondary: "#2bbde1",
-          card: {
-            background: "#0e0e0e",
-            borderRadius: 10,
-            hasShadow: true,
-            padding: 45,
-          },
-          typography: {
-            heading1: {
-              fontFamily: "Inter",
-              fontSize: 37,
-              fontWeight: 400,
-              color: "#ffffff",
-            },
-            heading2: {
-              fontFamily: "Inter",
-              fontSize: 29,
-              fontWeight: 200,
-              color: "#ffffff",
-            },
-            heading3: {
-              fontFamily: "Manrope",
-              fontSize: 20,
-              fontWeight: 600,
-              color: "#ffffff",
-            },
-            heading4: {
-              fontFamily: "Manrope",
-              fontSize: 18,
-              fontWeight: 800,
-              color: "#ffffff",
-            },
-            heading5: {
-              fontFamily: "Public Sans",
-              fontSize: 17,
-              fontWeight: 500,
-              color: "#ffffff",
-            },
-            heading6: {
-              fontFamily: "Public Sans",
-              fontSize: 14,
-              fontWeight: 400,
-              color: "#d1d1d1",
-            },
-            text: {
-              fontFamily: "Public Sans",
-              fontSize: 16,
-              fontWeight: 400,
-              color: "#ffffff",
-            },
-            link: {
-              fontFamily: "Inter",
-              fontSize: 16,
-              fontWeight: 400,
-              color: "#2bbde1",
-            },
-          },
-        },
-      }}
-    >
+    <EmbedProvider settings={{ ...embedSettings }}>
       <div className="flex justify-center items-center">
         <Checkout />
       </div>
--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { EmbedProvider, PricingTable } from "@schematichq/schematic-components";
+import { embedSettings } from "../embedSettings";
 
 export default function Pricing() {
   const apiKey = process.env.NEXT_PUBLIC_SCHEMATIC_PUBLISHABLE_KEY;
@@ -12,69 +13,7 @@ export default function Pricing() {
       apiKey={apiKey}
       apiConfig={apiConfig}
       settings={{
-        theme: {
-          numberOfColumns: 2,
-          sectionLayout: "merged",
-          colorMode: "light",
-          primary: "#2bbde1",
-          secondary: "#2bbde1",
-          card: {
-            background: "#0e0e0e",
-            borderRadius: 10,
-            hasShadow: true,
-            padding: 45,
-          },
-          typography: {
-            heading1: {
-              fontFamily: "Inter",
-              fontSize: 37,
-              fontWeight: 400,
-              color: "#ffffff",
-            },
-            heading2: {
-              fontFamily: "Inter",
-              fontSize: 29,
-              fontWeight: 200,
-              color: "#ffffff",
-            },
-            heading3: {
-              fontFamily: "Manrope",
-              fontSize: 20,
-              fontWeight: 600,
-              color: "#ffffff",
-            },
-            heading4: {
-              fontFamily: "Manrope",
-              fontSize: 18,
-              fontWeight: 800,
-              color: "#ffffff",
-            },
-            heading5: {
-              fontFamily: "Public Sans",
-              fontSize: 17,
-              fontWeight: 500,
-              color: "#ffffff",
-            },
-            heading6: {
-              fontFamily: "Public Sans",
-              fontSize: 14,
-              fontWeight: 400,
-              color: "#d1d1d1",
-            },
-            text: {
-              fontFamily: "Public Sans",
-              fontSize: 16,
-              fontWeight: 400,
-              color: "#ffffff",
-            },
-            link: {
-              fontFamily: "Inter",
-              fontSize: 16,
-              fontWeight: 400,
-              color: "#2bbde1",
-            },
-          },
-        },
+        ...embedSettings,
         badge: { alignment: "start", visibility: "visible" },
       }}
     >
