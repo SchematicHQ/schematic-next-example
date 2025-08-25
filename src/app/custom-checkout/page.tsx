1130e350f2f8fb3972dbcef44052abcab5eb6898
Joseph Chrzan <cole@schematichq.com>
2025-08-25 14:12:42 -0400

    chore(custom-checkout): add sample page (#65)
    * chore(custom-checkout): add sample page
    
    * chore: adjust
    
    * chore: cleanup
    
    * chore: move `setError` up
    
    * chore: use custom button; add custom checkout link to navbar
    
    * chore: adjust styling

---
+++ src/app/custom-checkout/page.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import {
+  CheckoutDialog,
+  EmbedProvider,
+  useEmbed,
+} from "@schematichq/schematic-components";
+import { useCallback, useEffect, useState } from "react";
+import { embedSettings } from "../embedSettings";
+
+function Checkout() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<Error>();
+
+  const { layout, stale, hydrate, setAccessToken, setLayout } = useEmbed();
+
+  const checkout = useCallback(async () => {
+    setError(undefined);
+    setIsLoading(true);
+
+    try {
+      const response = await fetch("/api/accessToken");
+      const result = await response.json();
+      if ("accessToken" in result) {
+        setAccessToken(result.accessToken);
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error);
+      }
+    } finally {
+      setIsLoading(false);
+      setLayout("checkout");
+    }
+  }, [setAccessToken, setLayout]);
+
+  useEffect(() => {
+    if (stale) {
+      hydrate();
+    }
+  }, [stale, hydrate]);
+
+  const buttonColor = error ? "red" : "cyan";
+  const loaderWidth = isLoading ? 4 : 0;
+
+  return (
+    <>
+      <button
+        className={`group appearance-none text-lg font-sans font-medium leading-none flex justify-center items-center w-fit px-6 py-4 rounded-lg text-black bg-${buttonColor}-500 border-transparent duration-100 hover:bg-${buttonColor}-400 hover:cursor-pointer`}
+        onClick={checkout}
+      >
+        <div
+          className={`w-${loaderWidth} h-${loaderWidth} mr-${loaderWidth / 2} rounded-full border-[${0.015625 * loaderWidth}rem] border-cyan-300 border-t-cyan-700 duration-100 animate-spin group-hover:border-cyan-200 group-hover:border-t-cyan-600`}
+        />
+        {error && (
+          <div className="text-xs font-sans font-black flex justify-center items-center w-4 h-4 mr-2 rounded-full text-red-500 bg-black">
+            !
+          </div>
+        )}
+        Checkout
+      </button>
+      {layout === "checkout" && <CheckoutDialog />}
+    </>
+  );
+}
+
+export default function CustomCheckout() {
+  return (
+    <EmbedProvider settings={{ ...embedSettings }}>
+      <div className="flex justify-center items-center">
+        <Checkout />
+      </div>
+    </EmbedProvider>
+  );
+}
